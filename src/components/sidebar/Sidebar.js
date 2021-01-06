7c6bf2e60f34ac159b11e5c21661df63b73ef19f
Luciano Molina <lucianoma63@gmail.com>
2021-01-06 03:25:23 -0300

    Elimine logos sidebar, junter users y estadisticas y ademas el logo de react cambia si se cambia en la api, pero hay que recargar 2 veces antes de cambiar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,42 +1,44 @@
 import React from 'react'
-import Logo from './AdminLTELogo.png';
-import Photo from './user.jpg';
+import {rutaAPI} from '../../config/Config';
 
-export default function Sidebar(){
 
-	const usuario = localStorage.getItem("USUARIO");
+export default function Sidebar()
+{
+		const dataLogo = async() => {
+		/*=============================================
+		=            CREAMOS EL DATASET               =
+		=============================================*/
+		
+		const getLogo = await getData();
+		console.log(getLogo.data[0].imagen)
+		localStorage.setItem("logo",getLogo.data[0].imagen)
+			
+		
+
+		}
+
+	dataLogo();
+	
 	return(
 
 		<aside className="main-sidebar sidebar-dark-primary elevation-4 ">
 
 			<a href="#/" className="brand-link">
-				<img alt="AdminLTE Logo"
+				<img alt="Ka Koach Logo"
 						className="brand-image img-circle elevation-3"
 						style={{opacity: 0.8}}
-						src={Logo}
+						src= {rutaAPI+"/mostrar-logo/"+localStorage.getItem("logo")}
 				/>
 
-				<span className="brand-text font-weight-light">CMS</span>
+				<span className="brand-text font-weight-light">Ka Koach</span>
 			</a>
 
 			<div className="sidebar">
 
-				<div className="user-panel mt-3 pb-3 mb-3 d-flex">
+			
 
-					<div className="image">
-						<img className="img-circle elevation-2"
-						alt="user"
-						src={Photo}
-						/>
-					</div>
 
-					<div className="info">
-						<a href="#/" className="d-block">
-						{usuario}
-						</a>
-					</div>
-
-			</div>		
+					
 					<div>
 
 						<nav className="mt-2">
@@ -45,12 +47,33 @@ export default function Sidebar(){
 								data-widget="treeview"
 								role="menu"
 								data-accordion="false"
-							>
+						>
+							<div className="sidenav-menu-heading">Negocio</div>
+							<li className="nav-item">
+								<a href="/" className="nav-link">
+									<i className="nav-icon fas fa-poll"></i>
+									<p>Ventas</p>
+									
+
+								</a>
+							</li>	
+						
+								<li className="nav-item">
+
+									<a href="/usuarios" className="nav-link">
+										<i className="nav-icon fas fa-users"></i>
+										<p>
+										Usuarios
+										</p>
+
+									</a>
+
+								</li>
 							<div class="sidenav-menu-heading">General</div>
 								<li className="nav-item">
 
 									<a href="/logo" className="nav-link">
-										<i className="nav-icon fas fa-user-lock"></i>
+										
 										<p>
 										Gestor Logo
 										</p>
@@ -62,7 +85,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/footer" className="nav-link">
-										<i className="nav-icon fas fa-sliders-h"></i>
+										
 										<p>
 										Gestor Footer
 										</p>
@@ -74,7 +97,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/redes_sociales" className="nav-link">
-										<i className="nav-icon fas fa-sliders-h"></i>
+										
 										<p>
 										Gestor Social Media
 										</p>
@@ -87,7 +110,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/inicio_slide" className="nav-link">
-										<i className="nav-icon fas fa-images"></i>
+										
 										<p>
 										Gestor Imagen Principal
 										</p>
@@ -99,7 +122,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/inicio_plan_personal" className="nav-link">
-										<i className="nav-icon fas fa-file"></i>
+									
 										<p>
 										Gestor Plan Primario
 										</p>
@@ -111,7 +134,7 @@ export default function Sidebar(){
 							    <li className="nav-item">
 
 									<a href="/inicio_planes_estrella" className="nav-link">
-										<i className="nav-icon fas fa-file"></i>
+										
 										<p>
 										Gestor Planes Secundarios
 										</p>
@@ -123,7 +146,7 @@ export default function Sidebar(){
 							    <li className="nav-item">
 
 									<a href="/inicio_benefits" className="nav-link">
-										<i className="nav-icon fas fa-file"></i>
+										
 										<p>
 										Gestor Beneficios
 										</p>
@@ -137,7 +160,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/planes_slide" className="nav-link">
-										<i className="nav-icon fas fa-users"></i>
+										
 										<p>
 										Gestor Img Principal
 										</p>
@@ -150,7 +173,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/planes_categoria" className="nav-link">
-										<i className="nav-icon fas fa-users"></i>
+										
 										<p>
 										Gestor Categorías
 										</p>
@@ -163,7 +186,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/planes" className="nav-link">
-										<i className="nav-icon fas fa-users"></i>
+										
 										<p>
 										Gestor Planes
 										</p>
@@ -175,7 +198,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/planes_benefits" className="nav-link">
-										<i className="nav-icon fas fa-users"></i>
+									
 										<p>
 										Gestor Beneficios
 										</p>
@@ -188,7 +211,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/sobre_mi_slide" className="nav-link">
-										<i className="nav-icon fas fa-users"></i>
+									
 										<p>
 										Gestor Img Principal
 										</p>
@@ -200,7 +223,7 @@ export default function Sidebar(){
 								<li className="nav-item">
 
 									<a href="/tarjetas" className="nav-link">
-										<i className="nav-icon fas fa-users"></i>
+									
 										<p>
 										Gestor Tarjetas
 										</p>
@@ -208,18 +231,7 @@ export default function Sidebar(){
 									</a>
 
 								</li>
-							<div className="sidenav-menu-heading">Usuarios</div>
-								<li className="nav-item">
 
-									<a href="/usuarios" className="nav-link">
-										<i className="nav-icon fas fa-users"></i>
-										<p>
-										Gestor Usuarios
-										</p>
-
-									</a>
-
-								</li>
 							</ul>
 						</nav>
 
@@ -232,4 +244,25 @@ export default function Sidebar(){
 
 		</aside>
 	)
+}
+/*=============================================
+=                     GET                     =
+=============================================*/
+	
+const getData = () => {
+	const url = `${rutaAPI}/show-data-logo`;
+	const params = {
+		method: "GET",
+		headers: {
+			"Content-type": "application/json"
+		}
+	}
+
+	return fetch(url, params).then(response => {
+		return response.json();
+	}).then(result => {
+		return  result;
+	}).then(err => {
+		return err;
+	})
 }
